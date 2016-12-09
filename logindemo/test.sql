ae4b51f44ba8c1b79d8d810f6189d83a0a50228f
mingmeng <yrx990327@163.com>
2016-12-10 00:24:50 +0800

    3rdhomework

---
+++ logindemo/test.sql
@@ -0,0 +1,5 @@
+INSERT INTO `test` VALUES (1, 'yang', 123456);
+INSERT INTO `test` VALUES (4, 'main', 223);
+INSERT INTO `test` VALUES (5, 'root', 23);
+INSERT INTO `test` VALUES (15, 'mingmeng', 123);
+INSERT INTO `test` VALUES (20, 'yan', 156);
